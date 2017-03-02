71a90dc22283291255de85d8f27a606cd9d93938
Ysa Tan <tanysaur@gmail.com>
2017-03-01 19:40:23 -0600

    Adding all files with uupdated Public

---
+++ models/users.js
@@ -0,0 +1,32 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    user_type: {
+      type: DataTypes.INTEGER(10).UNSIGNED,
+      allowNull: false,
+      defaultValue: 0.0
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+    updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+  });
+  return User;
+};
---
+++ todo.md
@@ -0,0 +1,16 @@
+## Front end
+
+### Sign-in
+ - Call AJAX on inputs of form (create a .js file to do all actions)
+ - Make it modal
+
+### Rent
+ - Refer to 'Eat-da-burger' homework
+ - Create a 'devour' button for your 'Rent' or 'Borrow'
+
+### Lend
+ - Create a form for user to fill out
+ - When 'done' is clicked show a list of products
+
+
+## Back end
